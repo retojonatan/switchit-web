fef07f0974d4ac6f6a94d696ae91787a39109afd
Pyrex <retojonatan@gmail.com>
2021-07-01 01:37:48 -0300

    added services

--- src/components/servicios.js
+++ src/components/servicios.js
@@ -1,50 +1,42 @@
 import { Link } from "gatsby";
 import { StaticImage } from "gatsby-plugin-image";
 import React, { Component } from "react";
-import { Button } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
+import * as serviceStyle from "./servicios.module.css";
 
 export default class Servicios extends Component {
   render() {
     return (
-      <div className="container">
-        <h2 className="display-3 fw-bold" style={{ lineHeight: `.5` }}>
+      <Container className="mt-n1 d-flex flex-column justify-content-center">
+        <h2 className={serviceStyle.subtitle + " display-3"}>
           Nos dedicamos a
         </h2>
-        <h1
-          className="display-1 fw-bolder"
-          style={{
-            color: `var(--sw-green)`,
-          }}
-        >
+        <h1 className={serviceStyle.title + " display-1"}>
           digitalizar marcas
         </h1>
-        <h5 className="pt-4 col-xl-10">
+        <h5 className={serviceStyle.paragraph + " col-xl-10"}>
           Brindamos un servicio integral de soluciones informáticas que, sumado
           al conocimiento a nuestra basta experiencia en gestión de negocios,
           nos permite brindarte la solución que tu negocio necesita para
           adaptarse a este nuevo mundo empresarial y crecer orgánicamente.
         </h5>
 
-        <div className="d-flex mt-4">
-          <Button
-            className="border-0 mt-4 mx-auto"
-            style={{ backgroundColor: "var(--sw-green)", borderRadius: "50px" }}
-            size="lg"
-          >
+        <div className="d-flex">
+          <Button className={serviceStyle.meetBtn} size="lg">
             <Link as="span" className="h3" to="/">
               Conocé nuestros servicios para
             </Link>
           </Button>
         </div>
         <StaticImage
-          src="../images/gatsby-astronaut.png"
+          src="../images/icons/desarrollo.svg"
           width={300}
-          quality={95}
+          quality={70}
           formats={["AUTO", "WEBP", "AVIF"]}
-          alt="A Gatsby astronaut"
+          alt="icono"
           style={{ marginBottom: `1.45rem` }}
         />
-      </div>
+      </Container>
     );
   }
 }
