af7c77fb1121181449de38f4ad27bdef5ce6b3bf
Pyrex <retojonatan@gmail.com>
2021-07-14 12:58:34 -0300

    added module components and slick

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import ReactFullpage from "@fullpage/react-fullpage";
 import * as React from "react";
+import Empresas from "../components/empresas";
 import Footer from "../components/footer";
 import Hero from "../components/hero";
+import Hogar from "../components/hogar";
+import InstitucionesEducativas from "../components/instituciones";
 import Layout from "../components/layout";
+import Nosotros from "../components/nosotros";
 import Seo from "../components/seo";
 import Servicios from "../components/servicios";
-import Background from "../images/home.png";
+import Background from "../images/headers/home.png";
+import Partners from "./../components/partners";
 
 const IndexPage = () => (
   <Layout>
@@ -26,6 +31,41 @@ const IndexPage = () => (
             </div>
             <div className="section">
               <Servicios />
+            </div>
+            <div
+              className="section"
+              style={{
+                backgroundColor: `var(--sw-green)`,
+              }}
+            >
+              <Hogar />
+            </div>
+            <div
+              className="section"
+              style={{
+                backgroundColor: `var(--sw-dark-pink)`,
+              }}
+            >
+              <InstitucionesEducativas />
+            </div>
+            <div
+              className="section"
+              style={{
+                backgroundColor: `var(--sw-yellow)`,
+              }}
+            >
+              <Empresas />
+            </div>
+            <div
+              className="section"
+              style={{
+                backgroundColor: `var(--sw-dark)`,
+              }}
+            >
+              <Partners />
+            </div>
+            <div className="section">
+              <Nosotros />
             </div>
             <div className="section">
               <footer className="h-100">
